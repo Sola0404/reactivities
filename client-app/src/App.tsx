c9b265cd8b44c1d4fa0cad29649c0dd4363d03dc
Yujia <0404sola@gmail.com>
2024-07-28 16:15:54 +1000

    Add CORS policy for client-app to allow requests from localhost:3000

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-	return <h1>Reactivities</h1>;
+	const [activities, setActivities] = useState([]);
+
+	useEffect(() => {
+		axios.get("http://localhost:5000/api/activities").then((response) => {
+			setActivities(response.data);
+		});
+	}, []);
+
+	return (
+		<div>
+			<h1>Reactivities</h1>
+			<ul>
+				{activities.map((activity: any) => (
+					<li key={activity.id}>{activity.title}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
