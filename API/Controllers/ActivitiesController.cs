0a302a21ee5c00ed19d37fa86230d6991a65bc5e
Yujia <0404sola@gmail.com>
2024-07-29 11:26:07 +1000

    Add a Create handler

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -17,5 +17,12 @@ namespace API.Controllers
         {
             return await Mediator.Send(new Details.Query { Id = id });
         }
+
+        [HttpPost] // api/activities
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok();
+        }
     }
 }
