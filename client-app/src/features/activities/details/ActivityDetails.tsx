3dee7132eb1f319c601453ed4eacc593bc419cf3
Yujia <0404sola@gmail.com>
2024-07-29 18:18:24 +1000

    Create ActivityDetails

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,27 @@
+import { Card, Image, Button } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+	activity: Activity;
+}
+
+export default function ActivityDetails({ activity }: Props) {
+	return (
+		<Card fluid>
+			<Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+			<Card.Content>
+				<Card.Header>{activity.title}</Card.Header>
+				<Card.Meta>
+					<span>{activity.date}</span>
+				</Card.Meta>
+				<Card.Description>{activity.description}</Card.Description>
+			</Card.Content>
+			<Card.Content extra>
+				<Button.Group widths="2">
+					<Button basic content="Edit" color="purple" />
+					<Button basic content="Cancel" color="grey" />
+				</Button.Group>
+			</Card.Content>
+		</Card>
+	);
+}
