8c40c84ea1887b6c45fa33dd4d71910494f65336
Yujia <0404sola@gmail.com>
2024-07-29 17:26:20 +1000

    Create an ActivityDashboard

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Container, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
 import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
 	const [activities, setActivities] = useState<Activity[]>([]);
@@ -19,11 +20,7 @@ function App() {
 		<>
 			<NavBar />
 			<Container style={{ marginTop: "7em" }}>
-				<List>
-					{activities.map((activity) => (
-						<List.Item key={activity.id}>{activity.title}</List.Item>
-					))}
-				</List>
+				<ActivityDashboard activities={activities} />
 			</Container>
 		</>
 	);
