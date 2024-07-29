2aa37c0cc3d3b110f6225f06811d33bbedaccc5c
Yujia <0404sola@gmail.com>
2024-07-29 18:02:02 +1000

    Create an ActivityList

---
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -0,0 +1,35 @@
+import { Button, Item, Label, Segment } from "semantic-ui-react";
+import { Activity } from "../../../app/models/activity";
+
+interface Props {
+	activities: Activity[];
+}
+
+export default function ActivityList({ activities }: Props) {
+	return (
+		<Segment>
+			<Item.Group divided>
+				{activities.map((activity) => (
+					<Item key={activity.id}>
+						<Item.Content>
+							<Item.Header as="a" className="activity-header">
+								{activity.title}
+							</Item.Header>
+							<Item.Meta>{activity.date}</Item.Meta>
+							<Item.Description>
+								<div>{activity.description}</div>
+								<div>
+									{activity.city}, {activity.venue}
+								</div>
+							</Item.Description>
+							<Item.Extra>
+								<Button floated="right" content="View" className="activity-btn" />
+								<Label basic content={activity.category} />
+							</Item.Extra>
+						</Item.Content>
+					</Item>
+				))}
+			</Item.Group>
+		</Segment>
+	);
+}
