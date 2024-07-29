c466ce6592bb228108bdcd7b7d5f75a597d4d68a
Yujia <0404sola@gmail.com>
2024-07-29 16:38:51 +1000

    Add a NavBar and some styles

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Header, List } from "semantic-ui-react";
+import { Container, List } from "semantic-ui-react";
 import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
 
 function App() {
 	const [activities, setActivities] = useState<Activity[]>([]);
@@ -15,14 +16,16 @@ function App() {
 	}, []);
 
 	return (
-		<div>
-			<Header as="h2" icon="users" content="Reactivities" />
-			<List>
-				{activities.map((activity) => (
-					<List.Item key={activity.id}>{activity.title}</List.Item>
-				))}
-			</List>
-		</div>
+		<>
+			<NavBar />
+			<Container style={{ marginTop: "7em" }}>
+				<List>
+					{activities.map((activity) => (
+						<List.Item key={activity.id}>{activity.title}</List.Item>
+					))}
+				</List>
+			</Container>
+		</>
 	);
 }
 
