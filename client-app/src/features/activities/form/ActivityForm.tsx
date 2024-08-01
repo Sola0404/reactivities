ea0cef34406dca77ef24593d4f5125413212c646
Yujia <0404sola@gmail.com>
2024-08-01 18:10:54 +1000

    Clean up unused code

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -2,7 +2,7 @@ import { Button, Form, Segment } from "semantic-ui-react";
 import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activity";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { v4 as uuid } from "uuid";
@@ -99,7 +99,13 @@ export default observer(function ActivityForm() {
 					content="Submit"
 					loading={loading}
 				/>
-				<Button floated="right" type="button" content="Cancel" />
+				<Button
+					as={Link}
+					to="/activities"
+					floated="right"
+					type="button"
+					content="Cancel"
+				/>
 			</Form>
 		</Segment>
 	);
