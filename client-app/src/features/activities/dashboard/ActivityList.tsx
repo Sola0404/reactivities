5700b8e805936520083b68e95154e2936af86888
Yujia <0404sola@gmail.com>
2024-08-02 22:01:54 +1000

    Create ActivityListItem component to refactor the code in ActivityList

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,58 +1,17 @@
-import { Button, Item, Label, Segment } from "semantic-ui-react";
-import { SyntheticEvent, useState } from "react";
+import { Item, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { Link } from "react-router-dom";
+import ActivityListItem from "./ActivityListItem";
 
 export default observer(function ActivityList() {
 	const { activityStore } = useStore();
-	const { deleteActivity, loading, activitiesBydate } = activityStore;
-
-	const [target, setTarget] = useState("");
-
-	function handleActivityDelete(
-		e: SyntheticEvent<HTMLButtonElement>,
-		id: string
-	) {
-		setTarget(e.currentTarget.name);
-		deleteActivity(id);
-	}
+	const { activitiesBydate } = activityStore;
 
 	return (
 		<Segment>
 			<Item.Group divided>
 				{activitiesBydate.map((activity) => (
-					<Item key={activity.id}>
-						<Item.Content>
-							<Item.Header as="a" className="activity-header">
-								{activity.title}
-							</Item.Header>
-							<Item.Meta>{activity.date}</Item.Meta>
-							<Item.Description>
-								<div>{activity.description}</div>
-								<div>
-									{activity.city}, {activity.venue}
-								</div>
-							</Item.Description>
-							<Item.Extra>
-								<Button
-									as={Link}
-									to={`/activities/${activity.id}`}
-									floated="right"
-									content="View"
-									className="activity-btn"
-								/>
-								<Button
-									name={activity.id}
-									onClick={(e) => handleActivityDelete(e, activity.id)}
-									floated="right"
-									content="Delete"
-									loading={loading && target === activity.id}
-								/>
-								<Label basic content={activity.category} />
-							</Item.Extra>
-						</Item.Content>
-					</Item>
+					<ActivityListItem key={activity.id} activity={activity} />
 				))}
 			</Item.Group>
 		</Segment>
