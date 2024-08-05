4a9ac82cfb5d18bfcdc898efc3d1864e2d1f6cfc
Yujia <0404sola@gmail.com>
2024-08-05 14:52:32 +1000

    Handle 400 errors

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -18,10 +18,20 @@ axios.interceptors.response.use(
 		return response;
 	},
 	(error: AxiosError) => {
-		const { data, status } = error.response!;
+		const { data, status } = error.response as AxiosResponse;
 		switch (status) {
 			case 400:
-				toast.error("Bad request");
+				if (data.errors) {
+					const modalStateErrors = [];
+					for (const key in data.errors) {
+						if (data.errors[key]) {
+							modalStateErrors.push(data.errors[key]);
+						}
+					}
+					throw modalStateErrors.flat();
+				} else {
+					toast.error(data);
+				}
 				break;
 			case 401:
 				toast.error("Unauthorized");
