30b6130af7766b355677d7e3be889594b9160164
Yujia <0404sola@gmail.com>
2024-08-05 11:33:49 +1000

    Add validator for edit and delete activity

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -15,14 +15,9 @@ namespace API.Controllers
 
         protected ActionResult HandleResult<T>(Result<T> result)
         {
-            if (result.IsSuccess && result.Value != null)
-            {
-                return Ok(result.Value);
-            }
-            if (result.IsSuccess && result.Value == null)
-            {
-                return NotFound();
-            }
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null) return NotFound();
             return BadRequest(result.Error);
         }
     }
