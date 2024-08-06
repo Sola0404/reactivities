ef7d155a591320d75de6b20bf9d7483f68c87334
Yujia <0404sola@gmail.com>
2024-08-06 18:16:17 +1000

    Create a reusable date picker

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -12,6 +12,7 @@ import MyTextInput from "../../../app/common/form/MyTextInput";
 import MyTextArea from "../../../app/common/form/MyTextArea";
 import MySelectInput from "../../../app/common/form/MySelectInput";
 import { categoryOptions } from "../../../app/common/options/categoryOptions";
+import MyDateInput from "../../../app/common/form/MyDateInput";
 
 export default observer(function ActivityForm() {
 	const { activityStore } = useStore();
@@ -83,7 +84,13 @@ export default observer(function ActivityForm() {
 							placeholder="Category"
 							name="category"
 						/>
-						<MyTextInput placeholder="Date" name="date" />
+						<MyDateInput
+							placeholderText="Date"
+							name="date"
+							showTimeSelect
+							timeCaption="time"
+							dateFormat="MMMM d, yyyy h:mm aa"
+						/>
 						<MyTextInput placeholder="City" name="city" />
 						<MyTextInput placeholder="Venue" name="venue" />
 						<Button
