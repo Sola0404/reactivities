d735e02ed666f36a5cde3159597420ea05c9c23c
Yujia <0404sola@gmail.com>
2024-08-11 13:18:01 +1000

    Add user login info in the homepage and navbar

--- client-app/src/features/home/HomePage.tsx
+++ client-app/src/features/home/HomePage.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function HomePage() {
+export default observer(function HomePage() {
+	const { userStore } = useStore();
 	return (
 		<Segment inverted textAlign="center" vertical className="masthead">
 			<Container text>
@@ -14,11 +17,19 @@ export default function HomePage() {
 					/>
 					Reactivities
 				</Header>
-				<Header as="h2" inverted content="Welcome to Reactivities" />
-				<Button as={Link} to="/login" size="huge" inverted>
-					Login!
-				</Button>
+				{userStore.isLoggedIn ? (
+					<>
+						<Header as="h2" inverted content="Welcome to Reactivities" />
+						<Button as={Link} to="/activities" size="huge" inverted>
+							Go to Activities!
+						</Button>
+					</>
+				) : (
+					<Button as={Link} to="/login" size="huge" inverted>
+						Login!
+					</Button>
+				)}
 			</Container>
 		</Segment>
 	);
-}
+});
