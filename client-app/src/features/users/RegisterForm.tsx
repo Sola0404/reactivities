d1afb4881100535363583aef644418b95d8f319f
Yujia <0404sola@gmail.com>
2024-08-11 15:09:28 +1000

    Handle validation errors in the register form

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -1,9 +1,10 @@
 import { ErrorMessage, Form, Formik } from "formik";
 import MyTextInput from "../../app/common/form/MyTextInput";
-import { Button, Header, Label } from "semantic-ui-react";
+import { Button, Header } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import * as Yup from "yup";
+import ValidationError from "../errors/ValidationError";
 
 export default observer(function RegisterForm() {
 	const { userStore } = useStore();
@@ -17,9 +18,7 @@ export default observer(function RegisterForm() {
 				error: null,
 			}}
 			onSubmit={(values, { setErrors }) =>
-				userStore
-					.register(values)
-					.catch(() => setErrors({ error: "Invalid email or password" }))
+				userStore.register(values).catch((error) => setErrors({ error }))
 			}
 			validationSchema={Yup.object({
 				displayName: Yup.string().required(),
@@ -43,12 +42,7 @@ export default observer(function RegisterForm() {
 					<ErrorMessage
 						name="error"
 						render={() => (
-							<Label
-								style={{ marginBottom: 10 }}
-								basic
-								color="pink"
-								content={errors.error}
-							/>
+							<ValidationError errors={errors.error as unknown as string[]} />
 						)}
 					/>
 					<Button
