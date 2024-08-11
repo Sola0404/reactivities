b4b9f7348f6a9614087a03814a0c722160f7df25
Yujia <0404sola@gmail.com>
2024-08-11 13:32:14 +1000

    Send up the token with the request

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,7 +15,7 @@ export default observer(function ActivityDashboard() {
 	}, [loadActivities, activityRegistry]);
 
 	if (activityStore.loadingInitial)
-		return <LoadingComponent content="Loading app..." />;
+		return <LoadingComponent content="Loading activities..." />;
 
 	return (
 		<Grid>
