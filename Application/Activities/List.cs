a552fb730e3688a7b6dfdd416a08c6dadd4c8b85
Yujia <0404sola@gmail.com>
2024-08-12 11:27:18 +1000

    Use Automapper to load attendees in getActivities and getActivity

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,6 +1,6 @@
 using Application.Core;
 using AutoMapper;
-using Domain;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -23,13 +23,10 @@ namespace Application.Activities
             public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
             {
                 var activities = await _context.Activities
-                .Include(a => a.Attendees)
-                .ThenInclude(u => u.AppUser)
-                .ToListAsync(cancellationToken);
+                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
 
-                var activitiesToReturn = _mapper.Map<List<ActivityDto>>(activities);
-
-                return Result<List<ActivityDto>>.Success(activitiesToReturn);
+                return Result<List<ActivityDto>>.Success(activities);
             }
         }
     }
