6ebddf1b68f5209bc90e5a7495fbc483a45752a4
Yujia <0404sola@gmail.com>
2024-08-13 18:09:19 +1000

    Add the store methods to join & cancel attendance

--- client-app/src/app/models/profile.ts
+++ client-app/src/app/models/profile.ts
@@ -1,6 +1,21 @@
-export interface Profile {
+import { User } from "./user";
+
+export interface IProfile {
 	username: string;
 	displayName: string;
 	image?: string;
 	bio?: string;
+}
+
+export class Profile implements IProfile {
+	username: string;
+	displayName: string;
+	image?: string;
+	bio?: string;
+
+	constructor(user: User) {
+		this.username = user.username;
+		this.displayName = user.displayName;
+		this.image = user.image;
+	}
 }
--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -3,6 +3,7 @@ import { Button, Header, Item, Segment, Image } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activity";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
+import { useStore } from "../../../app/stores/store";
 
 const activityImageStyle = {
 	filter: "brightness(30%)",
@@ -22,6 +23,9 @@ interface Props {
 }
 
 export default observer(function ActivityDetailedHeader({ activity }: Props) {
+	const {
+		activityStore: { updateAttendance, loading },
+	} = useStore();
 	return (
 		<Segment.Group>
 			<Segment basic attached="top" style={{ padding: "0" }}>
@@ -64,9 +68,13 @@ export default observer(function ActivityDetailedHeader({ activity }: Props) {
 						Manage Event
 					</Button>
 				) : activity.isGoing ? (
-					<Button>Cancel attendance</Button>
+					<Button loading={loading} onClick={updateAttendance}>
+						Cancel attendance
+					</Button>
 				) : (
-					<Button className="nav-btn">Join Activity</Button>
+					<Button loading={loading} onClick={updateAttendance} className="nav-btn">
+						Join Activity
+					</Button>
 				)}
 			</Segment>
 		</Segment.Group>
